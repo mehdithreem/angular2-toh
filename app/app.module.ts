db70e5ec2f48ec7e58cdcd04a37841930bc8e2c4
MMahdi Mahdizadeh <mehdithreem@gmail.com>
2017-02-06 12:57:15 +0330

    Tutorial Finished

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,6 +14,8 @@ import { AppComponent }  from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
+
 import { HeroService } from './hero.service';
 
 @NgModule({
@@ -28,7 +30,8 @@ import { HeroService } from './hero.service';
 		AppComponent,
 		HeroesComponent,
 		HeroDetailComponent,
-		DashboardComponent
+		DashboardComponent,
+		HeroSearchComponent
 	],
 	providers: [
 		HeroService
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
-
 @Component({
 	moduleId: module.id,
 	selector: 'my-dashboard',
